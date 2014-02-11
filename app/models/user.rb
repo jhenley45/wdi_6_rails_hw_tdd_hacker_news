38b6bedc3e397d4fe757f2b7fc2866af52922a55
Jack Henley <jack.d.henley@gmail.com>
2014-02-10 22:32:23 -0500

    Cleaned up unnecessary specs

--- app/models/user.rb
+++ app/models/user.rb
@@ -3,4 +3,9 @@ class User < ActiveRecord::Base
   # :confirmable, :lockable, :timeoutable and :omniauthable
   devise :database_authenticatable, :registerable,
          :recoverable, :rememberable, :trackable, :validatable
+
+  validates :email, presence: true
+  validates :username, presence: true
+
+  #has many :articles
 end
