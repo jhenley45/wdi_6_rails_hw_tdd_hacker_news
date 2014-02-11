1e727ad14fa62524c0ec63d077a76b591e4c72ba
Jack Henley <jack.d.henley@gmail.com>
2014-02-10 22:53:12 -0500

    Validating url for articles

--- spec/models/article_spec.rb
+++ spec/models/article_spec.rb
@@ -6,6 +6,10 @@ describe Article do
 		expect(Article.create(url: 'www.google.com', description: 'Hey yo its your boy da article')).to_not be_valid
 	end
 
+	it 'is invalid without a url' do
+		expect(Article.create(title: 'Google', description: 'Hey yo its your boy da article')).to_not be_valid
+	end
+
 
 
 end
