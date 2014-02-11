9ee5e2b8e4f6a55e7d3088a24a2999148d90359e
Jack Henley <jack.d.henley@gmail.com>
2014-02-10 22:35:47 -0500

    Validated email for users

--- spec/models/user_spec.rb
+++ spec/models/user_spec.rb
@@ -2,5 +2,8 @@ require 'spec_helper'
 
 describe User do
 
+	it 'is invalid without an email' do
+		expect(User.create(username: 'jhenley45', about: 'Hey yo its your boy')).to_not be_valid
+	end
 
 end
