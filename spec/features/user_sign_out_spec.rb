66cc45d140f26d776dc9585561dbba6d29725ace
Jack Henley <jack.d.henley@gmail.com>
2014-02-11 13:43:31 -0500

    Added spec for user logout

---
+++ spec/features/user_sign_out_spec.rb
@@ -0,0 +1,13 @@
+require 'spec_helper'
+
+feature 'User signs out' do
+	scenario 'successfully' do
+		user = create(:user)
+		sign_in_as(user)
+
+		click_link 'Sign Out'
+
+		expect(page).to_not have_content 'Signed in'
+	end
+
+end
