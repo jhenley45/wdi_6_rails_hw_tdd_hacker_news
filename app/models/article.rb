13cdf7d7beb8c45a37c260c25e99da5b7377ef3e
Jack Henley <jack.d.henley@gmail.com>
2014-02-10 22:52:12 -0500

    Validating for article title

--- app/models/article.rb
+++ app/models/article.rb
@@ -1,3 +1,6 @@
 class Article < ActiveRecord::Base
   belongs_to :user
+
+  validates :title, presence: true
+  validates :url, presence: true
 end
--- spec/models/article_spec.rb
+++ spec/models/article_spec.rb
@@ -2,4 +2,10 @@ require 'spec_helper'
 
 describe Article do
 
+	it 'is invalid without a title' do
+		expect(Article.create(url: 'www.google.com', description: 'Hey yo its your boy da article')).to_not be_valid
+	end
+
+
+
 end
