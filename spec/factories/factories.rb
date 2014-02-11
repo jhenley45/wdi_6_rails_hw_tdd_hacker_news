2c5a651fe4fba24097665f28f581968a08b232c8
Jack Henley <jack.d.henley@gmail.com>
2014-02-11 13:42:20 -0500

    Added successful sign in/out for users

--- spec/factories/factories.rb
+++ spec/factories/factories.rb
@@ -3,7 +3,6 @@ FactoryGirl.define do
 	factory :user do
 		email 'jack@example.com'
 		password 'password1'
-		username 'jhenley45'
 	end
 
 end
---
+++ spec/features/visitor_sign_in_spec.rb
@@ -0,0 +1,26 @@
+require 'spec_helper'
+
+feature 'Visitor signs in' do
+	background do
+		# FactoryGirl
+		@user = create(:user)
+		visit root_path
+	end
+	scenario 'successfully' do
+		fill_in 'Email', with: @user.email
+		fill_in 'Password', with: @user.password
+		click_button 'Sign in'
+
+		expect(page).to have_content "Signed in as #{@user.email}"
+		expect(page).to have_link 'Sign Out'
+	end
+
+	scenario 'unsuccessfully with invalid credentials' do
+		fill_in 'Email', with: @user.email
+		fill_in 'Password', with: 'wrongpassword'
+		click_button 'Sign in'
+
+		expect(page).to have_content 'Invalid email or password'
+		expect(page).to_not have_content 'Signed in'
+	end
+end
