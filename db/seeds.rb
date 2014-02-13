46878ccd14157ca2ccd4f3ebc287a63e67a0d8c5
Jack Henley <jack.d.henley@gmail.com>
2014-02-13 14:21:28 -0500

    Added seed file

--- db/seeds.rb
+++ db/seeds.rb
@@ -6,5 +6,15 @@
 #   cities = City.create([{ name: 'Chicago' }, { name: 'Copenhagen' }])
 #   Mayor.create(name: 'Emanuel', city: cities.first)
 
-jack = User.create!(id: 100, email: 'jack@yahoo22.com', password: 'password', about: 'something')
+jack = User.create!(id: 101, email: 'jack@yahoo44.com', password: 'password', about: 'something')
 
+10.times do |i|
+  article = Article.create!(user_id: 101, title: Faker::Lorem.word, url: Faker::Internet.url, description: Faker::Lorem.paragraphs(i%10).join(' ') )
+  puts "Created Article #{i}"
+
+  rand(5).times do |i|
+    the_user = User.find(101)
+    article.comments.create!(content: Faker::Lorem.paragraphs(i+1).join(' '), user: the_user)
+    puts "Created comment for #{article.title} "
+  end
+end
