fee857dd9e60597a6273cf04dcd4b88f5644cf7b
Jack Henley <jack.d.henley@gmail.com>
2014-02-13 16:38:52 -0500

    Added includes(:user) in comments controller

--- Gemfile
+++ Gemfile
@@ -49,4 +49,5 @@ group :development do
   gem 'rails_best_practices', require: false
   gem 'rails-footnotes'
   gem 'smusher'
+  gem "bullet"
 end
--- app/models/comment.rb
+++ app/models/comment.rb
@@ -1,7 +1,8 @@
 class Comment < ActiveRecord::Base
   belongs_to :user
-  belongs_to :article
+  belongs_to :article, #counter_cache: true
   has_many :votes, as: :votable
+
 
   validates :user_id, presence: true
   validates :article_id, presence: true
