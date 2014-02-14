c6127228554d7a58f2194782f09c1776cfc516de
Jack Henley <jack.d.henley@gmail.com>
2014-02-14 17:00:25 -0500

    Added specs for downvoting on comments

---
+++ spec/features/user_downvote_comment_spec.rb
@@ -0,0 +1,29 @@
+require 'spec_helper'
+
+
+feature 'Users can downvote article comments' do
+
+
+	background do
+		user = create(:user)
+		sign_in_as(user)
+		google = create(:article, title: 'Google', url: 'http://www.google.com/intl/en/about/', created_at: '2014-02-11 18:50:45', user: user)
+		visit articles_path
+		click_link 'comments'
+		fill_in 'Comment', with: 'This is a comment about Google.'
+		click_button 'Submit Comment'
+	end
+
+	scenario 'successfully' do
+		click_link 'downvote'
+		expect(page).to have_content '-1 points'
+	end
+
+	scenario 'unsuccessfully' do
+		click_link 'downvote'
+		expect(page).to have_content '-1 points'
+		click_link 'downvote'
+		expect(page).to have_content '-1 points'
+	end
+
+end
